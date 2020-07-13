6f27942c1c0b9c1dcbe64decfa968bef7a8c4ecd
André Van Dal <me@andrevandal.dev>
2020-07-12 22:45:09 -0300

    feat: add stylelint-config-prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  extends: ['stylelint-config-standard', 'stylelint-selector-bem-pattern'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-selector-bem-pattern',
+    'stylelint-config-prettier',
+  ],
   rules: {
     'at-rule-no-unknown': [
       true,
