7f72fe707d4f021b94c198f715a724c93b926ee3
André Van Dal <me@andrevandal.dev>
2020-07-12 22:38:46 -0300

    fix: tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,66 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
   theme: {
     colors: {
+      bluewood: {
+        100: '#E9EBED',
+        200: '#C9CDD1',
+        300: '#A8AFB5',
+        400: '#66747E',
+        500: '#253846',
+        600: '#21323F',
+        700: '#16222A',
+        800: '#111920',
+        900: '#0B1115',
+      },
+      oceangreen: {
+        100: '#EDF5F2',
+        200: '#D1E7DF',
+        300: '#B5D9CC',
+        400: '#7EBCA6',
+        500: '#479F80',
+        600: '#408F73',
+        700: '#2B5F4D',
+        800: '#20483A',
+        900: '#153026',
+      },
+      gray: {
+        100: '#F4F4F4',
+        200: '#E4E4E3',
+        300: '#D3D3D2',
+        400: '#B3B2B1',
+        500: '#92918F',
+        600: '#838381',
+        700: '#585756',
+        800: '#424140',
+        900: '#2C2C2B',
+      },
+    },
+    extend: {
       textIndent: {
-        // defaults to {}
         '1': '0.25rem',
         '2': '0.5rem',
       },
       textStyles: (theme) => ({
-        // defaults to {}
         heading: {
-          output: false, // this means there won't be a "heading" component in the CSS, but it can be extended
+          output: false,
           fontWeight: theme('fontWeight.bold'),
           lineHeight: theme('lineHeight.tight'),
         },
         h1: {
-          extends: 'heading', // this means all the styles in "heading" will be copied here; "extends" can also be an array to extend multiple text styles
+          extends: 'heading',
           fontSize: theme('fontSize.5xl'),
           '@screen sm': {
             fontSize: theme('fontSize.6xl'),
@@ -99,39 +143,6 @@ module.exports = {
           },
         },
       }),
-      bluewood: {
-        100: '#E9EBED',
-        200: '#C9CDD1',
-        300: '#A8AFB5',
-        400: '#66747E',
-        500: '#253846',
-        600: '#21323F',
-        700: '#16222A',
-        800: '#111920',
-        900: '#0B1115',
-      },
-      oceangreen: {
-        100: '#EDF5F2',
-        200: '#D1E7DF',
-        300: '#B5D9CC',
-        400: '#7EBCA6',
-        500: '#479F80',
-        600: '#408F73',
-        700: '#2B5F4D',
-        800: '#20483A',
-        900: '#153026',
-      },
-      gray: {
-        100: '#F4F4F4',
-        200: '#E4E4E3',
-        300: '#D3D3D2',
-        400: '#B3B2B1',
-        500: '#92918F',
-        600: '#838381',
-        700: '#585756',
-        800: '#424140',
-        900: '#2C2C2B',
-      },
     },
   },
   variants: {},
@@ -145,15 +156,4 @@ module.exports = {
       componentPrefix: '', // the prefix to use for text style classes
     }),
   ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-    ],
-  },
 }
