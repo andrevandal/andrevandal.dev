236acc3eb7461b6a6b1e4a70d7bd8d979bb56589
André Van Dal <me@andrevandal.dev>
2020-09-01 21:13:50 -0300

    feat: add MContainer with styles

---
+++ src/components/Molecues/MContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    :class="[
+      'container',
+      {
+        'container--justify-between': justify === 'between',
+        'container--items-center': items === 'center',
+      },
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    justify: {
+      type: String,
+      default: 'between',
+      validator: (value) => ['between'].includes(value),
+    },
+    items: {
+      type: String,
+      default: 'center',
+      validator: (value) => ['center'].includes(value),
+    },
+  },
+})
+</script>
+
+<style lang="postcss">
+.container {
+  @apply flex;
+  &--justify-between {
+    @apply justify-between;
+  }
+  &--items-center {
+    @apply items-center;
+  }
+}
+</style>
