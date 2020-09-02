592c336281cb934e186fc0b0eff26eba43dcb009
André Van Dal <me@andrevandal.dev>
2020-09-01 21:08:53 -0300

    feat: rename ONavbar

---
+++ src/components/Organisms/ONavbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar">
+    <MContainer items="center" justify="between">
+      <ALogo title="Navegar até a Página Inicial" />
+      <AHamburguer v-model="openned" class="relative z-20" />
+      <LazyMMenu v-show="openned" v-model="openned" />
+    </MContainer>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ONavbar',
+  data: () => ({
+    openned: false,
+  }),
+  head() {
+    return {
+      bodyAttrs: {
+        class: (this.openned && 'overflow-y-hidden') || '',
+      },
+    }
+  },
+})
+</script>
+
+<style lang="postcss">
+.navbar {
+  @apply flex justify-center py-16;
+  .container {
+    @apply flex justify-between items-center;
+  }
+}
+</style>
