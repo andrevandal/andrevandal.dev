63f05fde46fd77bc89fc1ecce7d7202d42d10325
André Van Dal <me@andrevandal.dev>
2020-09-01 21:06:42 -0300

    feat: add AIcon with style

---
+++ src/components/Atoms/AIcon.vue
@@ -0,0 +1,64 @@
+<template>
+  <svg-icon
+    :class="computedClass"
+    :name="name"
+    :title="title"
+    :desc="description"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+const allowedSizes = ['sm', 'md', 'lg']
+
+export default Vue.extend({
+  name: 'AtomIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: 'md',
+      validator: (value) => allowedSizes.includes(value),
+    },
+  },
+  computed: {
+    computedClass() {
+      return [
+        'icon',
+        {
+          'icon--sm': this.size === 'sm',
+          'icon--lg': this.size === 'lg',
+          'icon--md': this.size === 'md',
+        },
+      ]
+    },
+  },
+})
+</script>
+
+<style lang="postcss">
+.icon {
+  @apply fill-current;
+  &--sm {
+    @apply w-2 h-2;
+  }
+  &--md {
+    @apply w-4 h-4;
+  }
+  &--lg {
+    @apply w-8 h-8;
+  }
+}
+</style>
